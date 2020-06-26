4311bf76053460d68062aa301f5b730660318c1d
Hannes Achleitner <hannes.software@gmx.at>
2020-06-26 17:46:02 +0200

    Android X

--- app/src/main/java/com/example/android/tflitecamerademo/Home/HomeActivity.java
+++ app/src/main/java/com/example/android/tflitecamerademo/Home/HomeActivity.java
@@ -2,8 +2,8 @@ package com.example.android.tflitecamerademo.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import android.view.View;
 
 import com.example.android.tflitecamerademo.R;
