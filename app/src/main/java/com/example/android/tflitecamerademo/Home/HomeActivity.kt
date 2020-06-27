c0d48f82a61613614ef6529654b99dad2c1ff346
Hannes Achleitner <hannes.software@gmx.at>
2020-06-27 07:35:03 +0200

    Kotlin conversion

---
+++ app/src/main/java/com/example/android/tflitecamerademo/Home/HomeActivity.kt
@@ -0,0 +1,35 @@
+package com.example.android.tflitecamerademo.Home
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.tflitecamerademo.R
+import com.example.android.tflitecamerademo.RecognitionFirebase.RecognitionFirebaseActivity
+import com.example.android.tflitecamerademo.RecognitionObjects.CameraActivity
+import com.example.android.tflitecamerademo.RecognitionObjectsTensorFlow.Camera2Activity
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        cardInformation.setOnClickListener {
+            val intent = Intent(this@HomeActivity, AboutPageDevelopers::class.java)
+            startActivity(intent)
+        }
+        cardHand.setOnClickListener {
+            val intent = Intent(this@HomeActivity, Camera2Activity::class.java)
+            startActivity(intent)
+        }
+        cardObject.setOnClickListener {
+            val intent = Intent(this@HomeActivity, CameraActivity::class.java)
+            startActivity(intent)
+        }
+        cardObjectFirebase.setOnClickListener {
+            val intent = Intent(this@HomeActivity, RecognitionFirebaseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
